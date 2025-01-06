ad5391a8c9e9f0ea75a613dc5a0afe4eeb76e664
Aman <amandiv2345@gmail.com>
2025-01-06 21:59:07 +0530

    update

--- Largest_in_array_/Second_largest.java
+++ Largest_in_array_/Second_largest.java
@@ -1,3 +1,28 @@
+/*
+ *Intuition and Approach:
+Intuition:
+
+First, find the largest element in the array.
+Then, traverse the array again to find the largest element that is not equal to the previously found largest element.
+Approach:
+
+Traverse the array to find the largest element (largest).
+Traverse the array again to find the largest element smaller than largest (second_largest).
+Edge Cases:
+
+Array with fewer than 2 elements (e.g., {} or {5}).
+Array where all elements are identical (e.g., {3, 3, 3}).
+Time Complexity:
+
+First loop: O(n)
+Second loop: O(n)
+Total: O(2n) = O(n).
+Space Complexity:
+
+No additional space used → O(1).
+
+ */
+
 public class Second_largest {
     public static void main(String args[]) {
         int arr[] = {1, 3, 2, 4, 6, 5};
